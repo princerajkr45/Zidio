e9d3ce31f8ec866d19ee8d0b83ba7cd7a2d1e6d0
Prince Kumar <priyance5204@gmail.com>
2025-03-19 19:17:19 +0530

    frontend setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,37 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import {Route , Routes , BrowserRouter as Router } from 'react-router-dom'
+import Dashboard from './components/Dashboard'
+import AllTasks  from './pages/AllTasks'
+import ImportantTasks from './pages/ImportantTasks'
+import CompletedTasks from './pages/CompletedTasks'
+import IncompletedTasks from './pages/ImportantTasks'
+import Signup from './pages/SignUp'
+import Login from './pages/Login'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className="bg-gray-900 h-full text-white p-2 relative">
+        <Router>
+          <Routes>
+            <Route expct path="/" element={<Signup />}>
+              <Route index element={<AllTasks />} />
+              <Route path='/importantTasks' element={<ImportantTasks />} />
+              <Route path='/completedTasks' element={<CompletedTasks />} />
+              <Route path='/incompletedTasks' element={<IncompletedTasks />} />
+              <Route path='/login' element={<Login />} />
+              <Route path='/dashboard' element={<Dashboard />} />
+            </Route>
+          </Routes>
+        </Router>
+      </div>
+    </>
+  )
+}
+
+export default App
